50066fbee4b57d30e3e912544aaa877f772e8cee
SAYDOG <55991841+saydog@users.noreply.github.com>
2020-02-28 08:56:54 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@
 ########################################################
 ########################################################
 
-r="\e[31;1m"
-b="\e[36;1m"
-w="\e[00m"
+r="\x1b[31;1m"
+b="\x1b[36;1m"
+w="\x1b[00m"
 
 # Just animation
 cat << EOF
@@ -42,25 +42,25 @@ spin(){
 }
 
 count
-echo "\e[32m[*] Preparing for installing perl ..."
-echo "\e[00m    If u find (y/n) please choose Y or yes"
-echo "\e[00m    If u find manual, please type 'manual' and enter"
-echo "\e[32m[*] Installing perl ..."
-echo "\e[00m    Please wait in 10-15 minutes ..."
+echo "\x1b[32m[*] Preparing for installing perl ..."
+echo "\x1b[00m    If u find (y/n) please choose Y or yes"
+echo "\x1b[00m    If u find manual, please type 'manual' and enter"
+echo "\x1b[32m[*] Installing perl ..."
+echo "\x1b[00m    Please wait in 10-15 minutes ..."
 pkg install -y perl;pkg install -y clang;pkg install -y make;pkg install -y openssl;pkg install openssl-tool;cpan install Net::SSLeay::Handle;cpan LWP::UserAgent
-echo "\e[32m[*] Preparing for installing other packages ..."
-echo "\e[00m    Php, python, python2, ruby, nodejs, jp2a"
-echo "\e[00m    figlet, toilet, gnupg, zip, unzip,  wget"
+echo "\x1b[32m[*] Preparing for installing other packages ..."
+echo "\x1b[00m    Php, python, python2, ruby, nodejs, jp2a"
+echo "\x1b[00m    figlet, toilet, gnupg, zip, unzip,  wget"
 pkg install -y php python python2 ruby nodejs;pkg install -y jp2a figlet toilet gnupg zip unzip wget
-echo "\e[32m[*] Preparing for installing python requirements ..."
-echo "\e[00m    requests, bs4, pyfiglet, urllib"
+echo "\x1b[32m[*] Preparing for installing python requirements ..."
+echo "\x1b[00m    requests, bs4, pyfiglet, urllib"
 pip install requests bs4 pyfiglet urllib;pip2 install requests bs4;pip install --upgrade pip
-echo "\e[32m[*] All dependencies have been installed"
-echo "\e[32m[*] configure console calls"
-echo "\e[00m    Please wait"
+echo "\x1b[32m[*] All dependencies have been installed"
+echo "\x1b[32m[*] configure console calls"
+echo "\x1b[00m    Please wait"
 chmod +x run saydog;mv saydog /data/data/com.termux/files/usr/bin;rm install.sh
-echo "\e[32m[*] Saydog framework have been installed !"
-echo "\e[00m    thankyou for installing saydog framework version 1.2"
-echo "\e[00m    if u have any question, and if u find the bug in this tool"
-echo "\e[00m    plese contact to me saydog.official@gmail.com"
-echo "\e[00m    Start saydog framework with command: \e[33m./run\e[00m  or \e[33msaydog"
+echo "\x1b[32m[*] Saydog framework have been installed !"
+echo "\x1b[00m    thankyou for installing saydog framework version 1.2"
+echo "\x1b[00m    if u have any question, and if u find the bug in this tool"
+echo "\x1b[00m    plese contact to me saydog.official@gmail.com"
+echo "\x1b[00m    Start saydog framework with command: \x1b[33m./run\x1b[00m  or \x1b[33msaydog"
